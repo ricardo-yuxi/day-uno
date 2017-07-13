78cb1d86878b1085f2cab6f15f838638221ae052
Ricardo Rojas <reddrum7@gmail.com>
2017-07-13 16:37:47 -0500

    exercise first day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my application';
+  author = 'Ricardo Rojas';
+  count = 0;
+  imgSource = './../favicon.ico';
+  enabled = false;
+
+  increment() {
+    this.count = this.count + 1;
+  }
+
+  isEnabled() {
+    if (this.enabled) {
+      this.enabled = false;
+    } else {
+      this.enabled = true;
+    }
+  }
 }
